e259a5f65bd65f737d7dc078b8aad9fab83bcc54
Michael <michaelnares@hotmail.co.uk>
2013-10-27 22:32:44 +0000

    first commit of src

---
+++ CodewarsJava/src/CodewarsJava.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael
+ * Date: 27/10/2013
+ * Time: 22:25
+ * To change this template use File | Settings | File Templates.
+ */
+import org.junit.Test;
+
+public class CodewarsJava {
+
+        int correctAnswers = 0;
+        int failed = 0;
+
+    }
+
+
+
---
+++ CodewarsJava/src/CodewarsJavaTest.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+    /**
+     * Created with IntelliJ IDEA.
+     * User: michael
+     * Date: 27/10/2013
+     * Time: 19:33
+     * To change this template use File | Settings | File Templates.
+     */
+    public class CodewarsJavaTest {
+
+        @Before
+        public void setUp() throws Exception {
+
+        }
+
+        @Test
+
+        //Insert test methods and logging here.
+
+
+        @After
+        public void tearDown() throws Exception {
+
+        }
+    }
+
+
